33037a7e559fcb1f67f58dcb8ece9b586d492960
Joseph Gallagher <joseph.gallagher340@gmail.com>
2021-08-01 23:50:03 -0700

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -14,6 +14,7 @@ if (cold) {
 } 
 */
 
+/*
 const temperature = 0; 
 
 if (temperature <= 0) {
@@ -24,3 +25,33 @@ if (temperature <= 0) {
   console.log("Short sleeves are fine."); 
 }
 console.log("Now you're ready to go outside!"); 
+*/
+
+/*
+const isCitizen = false; 
+const age = 22; 
+
+if (isCitizen && age >= 18) {
+  console.log("You are eligible to vote."); 
+} else {
+  console.log("You can't vote!"); 
+}
+*/
+
+
+/*
+const temperature = 40; 
+
+if (temperature <= -40 || temperature >= 40) {
+  console.log("Maybe going outside isn't such a great idea..."); 
+}
+*/
+
+const raining = false; 
+
+if (!raining) {
+  console.log("Leave your umbrella at home!"); 
+} else {
+  console.log("Get your umbrella!"); 
+}
+
