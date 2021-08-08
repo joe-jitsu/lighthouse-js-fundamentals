cf9a11672a0f8c866449a1c5974ceca61c1b9765
Joseph Gallagher <joseph.gallagher340@gmail.com>
2021-08-08 11:17:04 -0700

    judgeVegetableFunction.js

---
+++ judgeVegetableFunction.js
@@ -0,0 +1,42 @@
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]; 
+
+const metric = 'redness'; 
+
+
+const judgeVegetable = function (vegetables, metric) {
+
+  let max = 0;   
+  let max2;   
+  for (let i = 0; i < vegetables.length; i++) {
+      
+    if (vegetables[i][metric] > max) {
+       
+      max = vegetables[i][metric]; 
+      max2 = vegetables[i].submitter;
+        
+    } 
+  
+  } 
+  return max2;    
+}
+
+console.log(judgeVegetable(vegetables, metric)); 
+
+
+
