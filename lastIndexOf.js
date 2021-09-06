5b76bc7de4ead22cd2fb9feaa8c4ffe95723091f
Joseph Gallagher <joseph.gallagher340@gmail.com>
2021-09-05 21:35:47 -0700

    lastIndexOf.js

--- lastIndexOf.js
+++ lastIndexOf.js
@@ -1,42 +1,24 @@
 
- 
- let numArray = [4, 6, 7, 19, 3, 8, 19, 3, 5, 1, 8, 3, 7]; 
+const lastIndexOf = function (array, value) {
 
- function lastIndexOf (array, value) {
-           
-   let output = []; 
+  let output = -1; 
+      
+  for (let i = 0; i < array.length; i ++) {
 
-   for (let i = array.length -1; i >= 0; i--) {
-   
       if (array[i] === value) {
 
-          output = [i]; 
+output = i; 
+               
+        }  
+    }      
+   return output;  
+}
 
-          return output; 
-      }
-
-   }  
-    
-      return -1;      
- }
-
-console.log(lastIndexOf(numArray, 3));   // 11
-console.log(lastIndexOf(numArray, 19));  // 6
-console.log(lastIndexOf(numArray, 4));   // 0
-console.log(lastIndexOf(numArray, 0));   // -1
-console.log(lastIndexOf(numArray, 17));   // -1
- 
-
-
-
-                     
-                     
-             
-
-
- 
-
-
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
 
 
 
