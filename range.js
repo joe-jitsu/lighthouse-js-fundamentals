b2dcfe2302ba7aa481107ac4e8599f51ae33f7af
Joseph Gallagher <joseph.gallagher340@gmail.com>
2021-09-06 00:10:50 -0700

    range.js

--- range.js
+++ range.js
@@ -1,22 +1,20 @@
-
-
 function range (start, end, step) {
-  
-  let output = [];
-  
-  for (let i = start; i < end;) {
-    
-    if (step === 0 || step < 0) {
-      return []; 
-    }  
-      
-    i = i + step;   
-    
+
+  let output = [];       
+
+  for (let i = start; i <= end; i = i + step) {
+
     output.push(i);  
+   
+    if (step === undefined) {
+
+      output = [];   
+    }
+          
   }
-  return output;    
+  return output;  
 }
 
-console.log(range(0, 8, 2));       
-
-console.log(range(-9, 3, 3));   
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
